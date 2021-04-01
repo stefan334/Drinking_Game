c198984d7b57ef0925e56be1830dd50adf06ece0
stefan334 <stefan.ivan334@gmail.com>
2021-04-01 17:55:31 +0300

    -Added questions and Intrebari.txt

--- app/src/main/java/com/example/splashscreen/gameActivity.java
+++ app/src/main/java/com/example/splashscreen/gameActivity.java
@@ -2,10 +2,16 @@ package com.example.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class gameActivity extends AppCompatActivity {
 
@@ -21,10 +27,29 @@ public class gameActivity extends AppCompatActivity {
 
         //hide status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        String[] nume = getIntent().getStringArrayExtra("NUME");
 
-        String[] nume = getIntent().getStringArrayExtra("NUME");   ///se poate stergge textviewu si astea era doar pt test
         TextView test= (TextView)findViewById(R.id.textView3);
-        test.setText(nume[1]);
+        Context mContext = this;
+
+        String[] intrebari= new String[100];
+        int i=0;
+        try {
+            InputStream is = mContext.getAssets().open("Intrebari.txt");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            String intrebare;
+            while((intrebare=reader.readLine())!=null){
+                intrebari[i++] = intrebare;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        test.setText(intrebari[0]);
+
+
+
+
+
 
     }
 }
