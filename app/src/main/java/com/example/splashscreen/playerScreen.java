b940beaf865aed2d63bb6c83d55b629a6e799ad6
stefan334 <stefan.ivan334@gmail.com>
2021-04-01 04:39:33 +0300

    -Added working remove buttons for each box.

--- app/src/main/java/com/example/splashscreen/playerScreen.java
+++ app/src/main/java/com/example/splashscreen/playerScreen.java
@@ -3,12 +3,14 @@ package com.example.splashscreen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Trace;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import static java.lang.Boolean.FALSE;
@@ -45,10 +47,20 @@ public class playerScreen extends AppCompatActivity {
          et[4]= findViewById(R.id.editTextTextPersonName4);
          et[5]= findViewById(R.id.editTextTextPersonName5);
          et[6]= findViewById(R.id.editTextTextPersonName6);
+        ImageButton but[]=new ImageButton[10]; ///vector de butoane de sters numele
+
+
+        but[4]= findViewById(R.id.removeButton3);
+        but[5]= findViewById(R.id.removeButton4);
+        but[6]= findViewById(R.id.removeButton5);
 
         et[4].setVisibility(View.GONE);
         et[5].setVisibility(View.GONE);
         et[6].setVisibility(View.GONE);
+        but[4].setVisibility(View.GONE);
+        but[5].setVisibility(View.GONE);
+        but[6].setVisibility(View.GONE);
+
         Button addPlayer =findViewById(R.id.addPlayer);
 
         addPlayer.setOnClickListener(new View.OnClickListener() {
@@ -58,11 +70,15 @@ public class playerScreen extends AppCompatActivity {
                 if(et[4].getVisibility()==View.GONE){ //  if (i==4)
                     et[4].setVisibility(View.VISIBLE);
                     butoane[2]= TRUE;
+                    but[4].setVisibility(View.VISIBLE);
+
                 }else if(et[5].getVisibility()==View.GONE &&et[4].getVisibility()==View.VISIBLE) {  //if(i==5)
                     et[5].setVisibility(View.VISIBLE);
+                    but[5].setVisibility(View.VISIBLE);
                     butoane[3]= TRUE;
                 }else if(et[6].getVisibility()==View.GONE&& et[5].getVisibility()==View.VISIBLE) { // if(i==6&&et[6].getVisibility()==View.GONE)
                     et[6].setVisibility(View.VISIBLE);
+                    but[6].setVisibility(View.VISIBLE);
                     butoane[4]= TRUE;
                 }else
                 if(et[6].getVisibility()==View.VISIBLE)  { //if(i==6&&et[6].getVisibility()==View.VISIBLE)
@@ -80,14 +96,18 @@ public class playerScreen extends AppCompatActivity {
             public void onClick(View view) {
                 if(et[6].getVisibility()==View.VISIBLE) {  //if(i==6&&et[6].getVisibility()==View.VISIBLE)
                     et[6].setVisibility(View.GONE);
+                    but[6].setVisibility(View.GONE);
                     butoane[4]= FALSE;
 
                 }else if(et[5].getVisibility()==View.VISIBLE&&et[6].getVisibility()==View.GONE){ //if(i==6&&et[6].getVisibility()==View.GONE)
                     et[5].setVisibility(View.GONE);
+                    but[5].setVisibility(View.GONE);
                     butoane[3]= FALSE;
 
                 }else if(et[4].getVisibility()==View.VISIBLE && et[5].getVisibility()==View.GONE){// if(i==5&& et[5].getVisibility()==View.VISIBLE)
                     et[4].setVisibility(View.GONE);
+                    but[4].setVisibility(View.GONE);
+
                     butoane[2]= FALSE;
 
                 }/*else if(i==5&& et[5].getVisibility()==View.GONE){
@@ -99,7 +119,9 @@ public class playerScreen extends AppCompatActivity {
                     errorToast.show();
                 }
 
+
             }
+
 
 
 
@@ -107,8 +129,6 @@ public class playerScreen extends AppCompatActivity {
         });
 
         startGame.setOnClickListener(new View.OnClickListener(){
-
-
 
             public void onClick(View view){
                 for(int i = 0; i<=4; i++){
@@ -124,6 +144,38 @@ public class playerScreen extends AppCompatActivity {
 
             }
         });
+        but[4].setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                et[4].setVisibility(View.GONE);
+                but[4].setVisibility(View.GONE);
+                butoane[4]=FALSE;
 
-    }
+
+            }
+            });
+            but[5].setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                et[5].setVisibility(View.GONE);
+                but[5].setVisibility(View.GONE);
+                butoane[5]=FALSE;
+
+
+            }
+            }); but[6].setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                et[6].setVisibility(View.GONE);
+                but[6].setVisibility(View.GONE);
+                butoane[6]=FALSE;
+
+
+            }
+            });
+
+
+
+        }
+
+
+
+
 }
