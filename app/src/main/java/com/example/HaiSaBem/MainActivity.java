abe5cea06248baafe9b7e4fba2b993deaa373087
stefan334 <stefan.ivan334@gmail.com>
2021-05-30 01:37:03 +0300

    -Fixed some more stuff
    Signed-off-by: stefan334 <stefan.ivan334@gmail.com>

--- app/src/main/java/com/example/HaiSaBem/MainActivity.java
+++ app/src/main/java/com/example/HaiSaBem/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.HaiSaBem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -10,6 +12,11 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_SCREEN = 3500;
@@ -24,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
 
         //hide action bar
         getSupportActionBar();
@@ -32,9 +42,9 @@ public class MainActivity extends AppCompatActivity {
         //hide status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        //se conecteaza animatiile la animatii.xml
         topAnimation= AnimationUtils.loadAnimation(this,R.anim.top_animation);
         bottomAnimation= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
 
         //se conecteaza id-urile la variabile
         image = findViewById(R.id.imageView);
@@ -55,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         },SPLASH_SCREEN);
+        //se conecteaza animatiile la animatii.xml
+
 
     }
 }
--- app/src/main/java/com/example/HaiSaBem/online_Lobby.java
+++ app/src/main/java/com/example/HaiSaBem/online_Lobby.java
@@ -1,22 +1,41 @@
 package com.example.HaiSaBem;
 
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.WindowManager;
-import android.widget.TextView;
 
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
+import com.google.firebase.dynamiclinks.ShortDynamicLink;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -24,11 +43,25 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.Map;
 import java.util.UUID;
 
 public class online_Lobby extends AppCompatActivity {
+
+    private AlertDialog.Builder dialogBuilder;
+    private AlertDialog dialog;
+    private EditText username;
+    private Button goButton,cancelButton;
+    private TextView player2Textview;
+    private TextView player3Textview;
+    private TextView player4Textview;
+    private TextView player1Textview;
+    private ArrayList<User> useri;
+    private int jucatori=1;
+    DocumentReference gameRef ;
+    Game game;
+
     private String replace_name(String intrebareInlocuita, ArrayList<String> nume){
         Collections.shuffle(nume);
         intrebareInlocuita=intrebareInlocuita.replaceAll("\\*", nume.get(0));
@@ -47,13 +80,236 @@ public class online_Lobby extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getSupportActionBar();
         getSupportActionBar().hide();
+        FirebaseFirestore db= FirebaseFirestore.getInstance();
+        gameRef = db.collection("games").document();
+        game=new Game();
         Context mContext = this;
         //hide status bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_online_lobby);
-        String player1 = getIntent().getStringExtra("NUME");
+        useri=new ArrayList<>();
 
-        TextView player1Textview;
+
+        FirebaseDynamicLinks.getInstance()
+                .getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        // Get deep link from result (may be null if no link is found)
+                        Uri deepLink = null;
+                        if (pendingDynamicLinkData != null) {
+                            deepLink = pendingDynamicLinkData.getLink();
+                            String link= deepLink.toString();
+                            link =link.substring(link.indexOf("=")+1);
+                            FirebaseFirestore db= FirebaseFirestore.getInstance();
+                            gameRef = db.collection("games").document(link);
+                            gameRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                                    @Nullable FirebaseFirestoreException e) {
+                                    if(e!=null){return;}
+
+                                    if (snapshot.exists()) {
+                                        //daca exista
+                                        updateView(snapshot);
+
+
+
+                                    }
+                                }
+                            });
+                            joinedGame(deepLink);
+
+                        }else {
+                            FirebaseFirestore db= FirebaseFirestore.getInstance();
+                            gameRef = db.collection("games").document();
+                            gameRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                                @Override
+                                public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                                    @Nullable FirebaseFirestoreException e) {
+                                    if(e!=null){return;}
+
+                                    if (snapshot.exists()) {
+                                        //daca exista
+                                        updateView(snapshot);
+
+
+
+                                    }
+                                }
+                            });
+                            hostCreated();
+
+                        }
+                        // Handle the deep link. For example, open the linked
+                        // content, or apply promotional credit to the user's
+                        // account.
+                        // ...
+
+                        // ...
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        FirebaseFirestore db= FirebaseFirestore.getInstance();
+                        gameRef = db.collection("games").document();
+                        gameRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                            @Override
+                            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                                @Nullable FirebaseFirestoreException e) {
+                                if(e!=null){return;}
+
+                                if (snapshot.exists()) {
+                                    //daca exista
+                                    updateView(snapshot);
+
+
+
+                                }
+                            }
+                        });
+                        hostCreated();
+
+                    }
+                });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    public void updateView(DocumentSnapshot snapshot){
+         useri = new ArrayList<>();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                    player2Textview= findViewById(R.id.player2Textview);
+                    player1Textview= findViewById(R.id.player1Textview);
+                    player3Textview= findViewById(R.id.player3Textview);
+                    player4Textview= findViewById(R.id.player4Textview);
+        gameRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                game.setPlayerNumber(Integer.valueOf(snapshot.get("playerNumber").toString()));
+                Map <String,String> user1 = (Map<String, String>) snapshot.get("player1");
+                Map <String,String> user2 = (Map<String, String>) snapshot.get("player2");
+                Map <String,String> user3 = (Map<String, String>) snapshot.get("player3");
+                Map <String,String> user4 = (Map<String, String>) snapshot.get("player4");
+
+                User _user1= new User();
+                _user1.setName(user1.get("name"));
+                _user1.setID(user1.get("id"));
+                game.setPlayer1(_user1);
+
+                User _user2= new User();
+                _user2.setName(user2.get("name"));
+                _user2.setID(user2.get("id"));
+                game.setPlayer2(_user2);
+
+                User _user3= new User();
+                _user3.setName(user3.get("name"));
+                _user3.setID(user3.get("id"));
+                game.setPlayer3(_user3);
+
+                User _user4= new User();
+                _user4.setName(user4.get("name"));
+                _user4.setID(user4.get("id"));
+                game.setPlayer4(_user4);
+
+                player1Textview.setText(game.getPlayer1().getName());
+
+                player2Textview.setText(game.getPlayer2().getName());
+
+                player3Textview.setText(game.getPlayer3().getName());
+
+                player4Textview.setText(game.getPlayer4().getName());
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
+    public void joinedGame(Uri deeplink){
+        dialogBuilder = new AlertDialog.Builder(this);
+        final View contactPopupView = getLayoutInflater().inflate(R.layout.popup, null);
+        username = (EditText) contactPopupView.findViewById(R.id.username);
+        goButton = (Button) contactPopupView.findViewById(R.id.goButton);
+        cancelButton = (Button) contactPopupView.findViewById(R.id.cancelButton);
+        dialogBuilder.setView(contactPopupView);
+        dialog = dialogBuilder.create();
+        dialog.show();
+
+
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();  ///te scoate din joc
+            }
+        });
+
+
+        goButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v){ ///te adauga in lobby
+                String name = username.getText().toString();
+                if(name.isEmpty()){
+                    Toast errorToast = Toast.makeText(online_Lobby.this, "Introduceti un nume!", Toast.LENGTH_SHORT);
+                    errorToast.show();
+                }else{
+                    UUID uuid = UUID.randomUUID();
+                    String randomUUIDString = uuid.toString();
+                    User user= new User();
+                    user.setID(randomUUIDString);
+                    user.setName(name);
+
+                    gameRef.update("player" + (game.getPlayerNumber()+1),user);
+                    gameRef.update("playerNumber",game.getPlayerNumber()+1);
+
+                    dialog.dismiss();
+
+            }
+
+
+            }
+        });
+
+
+
+
+    }
+    public void hostCreated(){
+        String player1 = getIntent().getStringExtra("NUME");
+        Context mContext = this;
+
         player1Textview = findViewById(R.id.player1Textview);
         player1Textview.setText(player1);
 
@@ -65,12 +321,14 @@ public class online_Lobby extends AppCompatActivity {
         User user= new User();
         user.setID(randomUUIDString);
         user.setName(player1);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-       // DocumentReference gameRef = db.collection("games").document();
-        Game game=new Game();
-        game.setUser(user);
 
-        ArrayList<String> intrebari= new ArrayList<String>();   ///declarare exact cate intrebari avem
+
+
+        Game game=new Game();
+        game.setPlayer1(user);
+        game.setPlayerNumber(1);
+
+        ArrayList<String> intrebari= new ArrayList<>();   ///declarare exact cate intrebari avem
 
         try {   ///aici se citeste din fisier
             InputStream is = mContext.getAssets().open("Intrebari.txt");
@@ -85,30 +343,45 @@ public class online_Lobby extends AppCompatActivity {
         intrebari=random_intrebari(intrebari);
 
         game.setIntrebariArrayList(intrebari);
-        
 
-        db.collection("games")
-                .add(game)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+
+        gameRef.set(game);
+
+
+
+        Task<ShortDynamicLink> shortLinkTask = FirebaseDynamicLinks.getInstance().createDynamicLink()
+                .setLink(Uri.parse("https://www.example.com/game_id=" +gameRef.getId()))
+                .setDomainUriPrefix("https://thunderstudios.page.link")
+                .setAndroidParameters(
+                        new DynamicLink.AndroidParameters.Builder("com.ThunderStudios.HaiSaBem")
+                                .setMinimumVersion(1)
+                                .build())
+                .setGoogleAnalyticsParameters(
+                        new DynamicLink.GoogleAnalyticsParameters.Builder()
+                                .setSource("orkut")
+                                .setMedium("social")
+                                .setCampaign("example-promo")
+                                .build())
+                .setSocialMetaTagParameters(
+                        new DynamicLink.SocialMetaTagParameters.Builder()
+                                .setTitle("Example of a Dynamic Link")
+                                .setDescription("This link works whether the app is installed or not!")
+                                .build())
+                .buildShortDynamicLink()
+                .addOnCompleteListener(this, new OnCompleteListener<ShortDynamicLink>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("FragmentActivity", "DocumentSnapshot added with ID: " + documentReference.getId());
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w("FragmentActivity", "Error adding document", e);
+                    public void onComplete(@NonNull Task<ShortDynamicLink> task) {
+                        if (task.isSuccessful()) {
+                            // Short link created
+                            System.out.println(task.getResult().getShortLink());
+                            Uri shortLink = task.getResult().getShortLink();
+                            Uri flowchartLink = task.getResult().getPreviewLink();
+                        } else {
+                            // Error
+                            // ...
+                        }
                     }
                 });
-
-
-
-
-
-
-
-
 
     }
 
